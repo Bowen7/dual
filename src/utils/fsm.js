2b3aa514a60242973535f14ef489092ad0dd46d6
张文翔 <771797746@qq.com>
2020-01-11 23:44:25 +0800

    feat: fsm参数transtions接受多种格式

--- src/utils/fsm.js
+++ src/utils/fsm.js
@@ -1,43 +1,55 @@
-import { isObject } from './helper'
-const fatalSymbol = Symbol('fatal')
+import { checkOptions, isObject } from './helper'
 class FSM {
   constructor(options = {}) {
-    if (!this.checkOptions(options)) {
+    if (!checkOptions(options, ['transitions'])) {
       return
     }
+  }
+  init(options) {
+    let _transitions = {}
     let { init = '', transitions = [] } = options
     if (!init) {
       init = transitions[0] && transitions[0].from
     }
-    this.state = init
-    this.transitions = transitions
-  }
-  checkOptions(options) {
-    const requiredOptions = ['transitions']
-    let checkResult = true
-    requiredOptions.forEach(key => {
-      if (!hasOwnProperty(options, key)) {
-        console.error(`options: ${key} is required`)
-        this.state = fatalSymbol
-        checkResult = false
+    if (isObject(transitions)) {
+      for (let key in transitions) {
+        const value = transitions[key]
+        if (isObject(value) && typeof value.to === 'string') {
+          _transitions[key] = {
+            to: [value.to],
+            input: [value.input]
+          }
+          continue
+        }
+        _transitions[key] = value
       }
-    })
-    return checkResult
-  }
-  check() {
-    if (this.state === fatalSymbol) {
-      console.error('fatal FSM. check it')
-      return false
+    } else if (Array.isArray(transitions)) {
+      transitions.forEach(item => {
+        if (isObject(item) && typeof item.to === 'string') {
+          _transitions[item.from] = {
+            to: [item.to],
+            input: [item.input]
+          }
+          return
+        }
+        _transitions[item.from] = {
+          to: item.to,
+          input: item.input
+        }
+      })
     }
-    return true
+    this.state = init
+    this.transitions = _transitions
   }
   transit(input) {
-    if (!this.check()) {
-      return
-    }
+    let resultState
+    this.transitions.forEach(transition => {
+      if (transition.from === this.state && transition.input === input) {
+        resultState = transition.to
+      }
+    })
+    this.state = resultState
+    return this.state
   }
-}
-function hasOwnProperty(target, key) {
-  return Object.prototype.hasOwnProperty.call(target, key)
 }
 export default FSM
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,3 +1,18 @@
 export const isObject = target => {
   return Object.prototype.toString.call(target) === '[object Object]'
 }
+
+export const hasOwnProperty = (target, key) => {
+  return Object.prototype.hasOwnProperty.call(target, key)
+}
+
+export const checkOptions = (options, requiredOptions = []) => {
+  let checkResult = true
+  requiredOptions.forEach(key => {
+    if (!hasOwnProperty(options, key)) {
+      console.error(`options: ${key} is required`)
+      checkResult = false
+    }
+  })
+  return checkResult
+}
