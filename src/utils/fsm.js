a9e6bf1932c697cd2ec3ce2932b8ac587d75b762
张文翔 <771797746@qq.com>
2020-01-12 23:30:44 +0800

    feat: fsm增加oberse功能

--- src/utils/fsm.js
+++ src/utils/fsm.js
@@ -1,55 +1,71 @@
 import { checkOptions, isObject } from './helper'
+function transformTransition(transition) {
+  if (isObject(transition) && typeof transition.to === 'string') {
+    return {
+      to: [transition.to],
+      input: [transition.input]
+    }
+  }
+  return {
+    to: transition.to,
+    input: transition.input
+  }
+}
 class FSM {
   constructor(options = {}) {
     if (!checkOptions(options, ['transitions'])) {
       return
     }
+    this._init(options)
+    this._observer = {}
   }
-  init(options) {
+  _init(options) {
     let _transitions = {}
     let { init = '', transitions = [] } = options
-    if (!init) {
-      init = transitions[0] && transitions[0].from
-    }
+    // transitions接受对象和数组
     if (isObject(transitions)) {
-      for (let key in transitions) {
-        const value = transitions[key]
-        if (isObject(value) && typeof value.to === 'string') {
-          _transitions[key] = {
-            to: [value.to],
-            input: [value.input]
-          }
-          continue
-        }
-        _transitions[key] = value
+      for (let from in transitions) {
+        !init && (init = from)
+        const value = transitions[from]
+        _transitions[from] = transformTransition(value)
       }
     } else if (Array.isArray(transitions)) {
       transitions.forEach(item => {
-        if (isObject(item) && typeof item.to === 'string') {
-          _transitions[item.from] = {
-            to: [item.to],
-            input: [item.input]
-          }
-          return
-        }
-        _transitions[item.from] = {
-          to: item.to,
-          input: item.input
-        }
+        const from = item.from
+        !init && (init = from)
+        _transitions[from] = transformTransition(item)
       })
     }
     this.state = init
     this.transitions = _transitions
   }
   transit(input) {
-    let resultState
-    this.transitions.forEach(transition => {
-      if (transition.from === this.state && transition.input === input) {
-        resultState = transition.to
-      }
+    // 可接受单个和多组input
+    if (!Array.isArray(input)) {
+      input = [input]
+    }
+    input.forEach(item => {
+      const curState = this.state
+      const transition = this.transitions[curState]
+      const { input: inputArr = [], to = [] } = transition
+      const index = inputArr.indexOf(item)
+      this.state = to[index]
+
+      // 触发监听函数
+      const observer = this._observer[this.state]
+      observer &&
+        observer.forEach(cb => {
+          cb()
+        })
     })
-    this.state = resultState
     return this.state
+  }
+  observe(state, cb) {
+    if (this._observer[state]) {
+      this._observer[state].push(cb)
+    } else {
+      this._observer[state] = [cb]
+    }
   }
 }
 export default FSM
